bec8734be4d01f4bbb1fc4e6bc763e446eabd80c
Katie McFaul <kmcfaul@redhat.com>
2025-07-15 10:46:40 -0400

    fix test

--- packages/module/src/MessageDivider/MessageDivider.test.tsx
+++ packages/module/src/MessageDivider/MessageDivider.test.tsx
@@ -9,16 +9,16 @@ describe('MessageDivider', () => {
   it('should render default correctly with variant = date and content = new Date().toLocaleDateString()', () => {
     render(<MessageDivider data-testid="message-divider" />);
     expect(screen.getByText(new Date().toLocaleDateString())).toBeInTheDocument();
-    expect(screen.getByTestId('message-divider').children[0]).toHaveClass('pf-chatbot__message-divider--date');
+    expect(screen.getByTestId('message-divider').children[0]).toHaveClass('pf-chatbot__message-divider--inset');
   });
   it('should render inset variant correctly', () => {
     render(<MessageDivider variant="inset" content="test" data-testid="message-divider" />);
     expect(screen.getByText('test')).toBeInTheDocument();
-    expect(screen.getByTestId('message-divider').children[0]).toHaveClass('pf-chatbot__message-divider--date');
+    expect(screen.getByTestId('message-divider').children[0]).toHaveClass('pf-chatbot__message-divider--inset');
   });
   it('should render fullWidth variant correctly', () => {
     render(<MessageDivider variant="fullWidth" content="test" data-testid="message-divider" />);
     expect(screen.getByText('test')).toBeInTheDocument();
-    expect(screen.getByTestId('message-divider')).toHaveClass('pf-chatbot__message-divider--announcement');
+    expect(screen.getByTestId('message-divider')).toHaveClass('pf-chatbot__message-divider--full-width');
   });
 });
