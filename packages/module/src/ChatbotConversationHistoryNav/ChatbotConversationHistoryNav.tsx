383b96a456c90c6249ca0746375b67695ec1a75c
Rebecca Alpert <ralpert@redhat.com>
2025-08-05 13:29:03 -0400

    fix(ChatbotConversationHistoryNav): Make title icon and heading customizable (#616)
    Also change spacing between icon and title to 0.5rem.

--- packages/module/src/ChatbotConversationHistoryNav/ChatbotConversationHistoryNav.tsx
+++ packages/module/src/ChatbotConversationHistoryNav/ChatbotConversationHistoryNav.tsx
@@ -84,7 +84,7 @@ export interface ChatbotConversationHistoryNavProps extends DrawerProps {
   /** Additional button props for new chat button. */
   newChatButtonProps?: ButtonProps;
   /** Additional props applied to all conversation list headers */
-  titleProps?: Partial<TitleProps>;
+  listTitleProps?: Partial<TitleProps>;
   /** Additional props applied to conversation list. If conversations is an object, you should pass an object of ListProps for each group. */
   listProps?: ListProps | { [key: string]: ListProps };
   /** Text shown in blue button */
@@ -137,6 +137,10 @@ export interface ChatbotConversationHistoryNavProps extends DrawerProps {
   isCompact?: boolean;
   /** Display title  */
   title?: string;
+  /** Icon displayed in title */
+  navTitleIcon?: React.ReactNode;
+  /** Title header level */
+  navTitleProps?: Partial<TitleProps>;
 }
 
 export const ChatbotConversationHistoryNav: FunctionComponent<ChatbotConversationHistoryNavProps> = ({
@@ -146,7 +150,7 @@ export const ChatbotConversationHistoryNav: FunctionComponent<ChatbotConversatio
   activeItemId,
   onSelectActiveItem,
   conversations,
-  titleProps,
+  listTitleProps,
   listProps,
   newChatButtonText = 'New chat',
   drawerContent,
@@ -173,6 +177,8 @@ export const ChatbotConversationHistoryNav: FunctionComponent<ChatbotConversatio
   noResultsState,
   isCompact,
   title = 'Chat history',
+  navTitleProps,
+  navTitleIcon = <OutlinedClockIcon />,
   ...props
 }: ChatbotConversationHistoryNavProps) => {
   const drawerRef = useRef<HTMLDivElement>(null);
@@ -225,7 +231,7 @@ export const ChatbotConversationHistoryNav: FunctionComponent<ChatbotConversatio
         <div>
           {Object.keys(conversations).map((navGroup) => (
             <section key={navGroup}>
-              <Title headingLevel="h4" className="pf-chatbot__conversation-list-header" {...titleProps}>
+              <Title headingLevel="h4" className="pf-chatbot__conversation-list-header" {...listTitleProps}>
                 {navGroup}
               </Title>
               <List className="pf-chatbot__conversation-list" isPlain {...listProps?.[navGroup]}>
@@ -286,13 +292,15 @@ export const ChatbotConversationHistoryNav: FunctionComponent<ChatbotConversatio
             )}
           </DrawerActions>
         </DrawerHead>
-        <div className="pf-chatbot__title-container">
-          <Title headingLevel="h3">
+        <div className="pf-chatbot__heading-container">
+          <div className="pf-chatbot__title-container">
             <Icon size="lg" className="pf-chatbot__title-icon">
-              <OutlinedClockIcon />
+              {navTitleIcon}
             </Icon>
-            {title}
-          </Title>
+            <Title className="pf-chatbot__title" headingLevel="h2" {...navTitleProps}>
+              {title}
+            </Title>
+          </div>
           {!isLoading && handleTextInputChange && (
             <div className="pf-chatbot__input">
               <SearchInput
