a9e1876d05c228484e8e893c352dbdaa50ba113a
Rebecca Alpert <ralpert@redhat.com>
2025-09-09 14:14:41 -0400

    feat(SourcesCardBase): Expose just card portion of SourcesCard
    Allows consumers to custom-roll their own layouts using preexisting component.

--- packages/module/src/index.ts
+++ packages/module/src/index.ts
@@ -90,14 +90,17 @@ export * from './SourceDetailsMenuItem';
 export { default as SourcesCard } from './SourcesCard';
 export * from './SourcesCard';
 
+export { default as SourcesCardBase } from './SourcesCardBase';
+export * from './SourcesCardBase';
+
 export { default as TermsOfUse } from './TermsOfUse';
 export * from './TermsOfUse';
 
-export { default as ToolResponse } from './ToolResponse';
-export * from './ToolResponse';
-
 export { default as ToolCall } from './ToolCall';
 export * from './ToolCall';
+
+export { default as ToolResponse } from './ToolResponse';
+export * from './ToolResponse';
 
 export { default as tracking } from './tracking';
 export * from './tracking';
